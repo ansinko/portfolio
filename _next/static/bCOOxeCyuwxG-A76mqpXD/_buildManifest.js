16a9e0e6946f3738007fc3aec16be2eb646de919
Andrej Meliška <andrej.meliska@innovis.sk>
2025-10-07 01:55:34 +0200

    Updates

---
+++ _next/static/bCOOxeCyuwxG-A76mqpXD/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/ae7ec82d46fee259.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio//_next/:path+",
+        "destination": "/portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
