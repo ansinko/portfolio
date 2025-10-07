a5dd48a8e680c0a3ce0d97e6538081186a9f08ed
Andrej Meliška <andrej.meliska@innovis.sk>
2025-10-07 14:57:48 +0200

    Refactor Tailwind CSS configuration and update dependencies; remove unused postcss config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-    output: 'export',
-    basePath: isProd ? '/portfolio' : '',
-    assetPrefix: isProd ? '/portfolio/' : '',
     images: {
         unoptimized: true,
     },
