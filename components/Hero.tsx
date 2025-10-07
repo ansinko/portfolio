2e82bc3ff4631e1020d9bc2576de9adeaf956407
Andrej Meliška <andrej.meliska@innovis.sk>
2025-10-07 16:28:46 +0200

    Refactor Spotlight component to use motion for animations and simplify Hero section usage

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,10 +2,10 @@
 
 import { FaLocationArrow } from 'react-icons/fa6';
 import MagicButton from './ui/MagicButton';
-import { Spotlight } from './ui/Spotlight';
 import { TextGenerateEffect } from './ui/TextGenerateEffect';
 import { useLanguage } from '@/contexts/LanguageContext';
 import LanguageToggle from './ui/LanguageToggle';
+import { Spotlight } from './ui/Spotlight';
 
 const Hero = () => {
     const { t } = useLanguage();
@@ -17,18 +17,7 @@ const Hero = () => {
              *  Link: https://ui.aceternity.com/components/spotlight
              */}
             <div>
-                <Spotlight
-                    className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen'
-                    fill='white'
-                />
-                <Spotlight
-                    className='h-[80vh] w-[50vw] top-10 left-full'
-                    fill='purple'
-                />
-                <Spotlight
-                    className='left-80 top-28 h-[80vh] w-[50vw]'
-                    fill='blue'
-                />
+                <Spotlight />
             </div>
 
             {/**
